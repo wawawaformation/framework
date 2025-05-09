ab7bd6ccc9a94b374ff81fd52c3978f6df142cd5
David LEGRAND <wawawaformation@gmail.com>
2025-05-09 11:43:22 +0200

    RESET

--- public/js/recaptcha.js
+++
@@ -1,9 +0,0 @@
-const formElt = document.querySelector('form')
-const rgpdElt = document.querySelector('#rgpd')
-
-formElt.addEventListener('submit', function (e) {
-  if (!rgpdElt.checked) {
-    e.preventDefault()
-    alert("Veuillez accepter la politique des données personnelles.")
-  }
-})
--- tests/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-// Charger les constantes du projet
-require_once dirname(__DIR__) . '/config/const.php';
-
-// Tu peux ajouter d'autres initialisations ici plus tard
